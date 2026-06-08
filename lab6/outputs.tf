927f6b1114224ed5eb2a2c4acd3447efbcc78a54
Eugene <bielichenko777eugene@gmail.com>
2026-06-08 19:23:07 +0200

    fix: exact names for lab6 outputs

--- lab6/outputs.tf
+++ lab6/outputs.tf
@@ -8,57 +8,39 @@ output "vpc_cidr" {
   value       = aws_vpc.vpc.cidr_block
 }
 
-output "subnet_a_id" {
-  description = "Provides subnet A ID."
-  value       = aws_subnet.subnet_a.id
+output "public_subnet_ids" {
+  description = "Provides IDs of all public subnets."
+  value = [
+    aws_subnet.subnet_a.id,
+    aws_subnet.subnet_b.id,
+    aws_subnet.subnet_c.id
+  ]
 }
 
-output "subnet_b_id" {
-  description = "Provides subnet B ID."
-  value       = aws_subnet.subnet_b.id
+output "public_subnet_cidr_block" {
+  description = "Provides CIDR blocks of all public subnets."
+  value = [
+    aws_subnet.subnet_a.cidr_block,
+    aws_subnet.subnet_b.cidr_block,
+    aws_subnet.subnet_c.cidr_block
+  ]
 }
 
-output "subnet_c_id" {
-  description = "Provides subnet C ID."
-  value       = aws_subnet.subnet_c.id
+output "public_subnet_availability_zone" {
+  description = "The Availability Zones of all public subnets."
+  value = [
+    aws_subnet.subnet_a.availability_zone,
+    aws_subnet.subnet_b.availability_zone,
+    aws_subnet.subnet_c.availability_zone
+  ]
 }
 
-output "subnet_a_cidr" {
-  description = "Provides subnet A cidr."
-  value       = aws_subnet.subnet_a.cidr_block
-}
-
-output "subnet_b_cidr" {
-  description = "Provides subnet B cidr."
-  value       = aws_subnet.subnet_b.cidr_block
-}
-
-output "subnet_c_cidr" {
-  description = "Provides subnet C cidr."
-  value       = aws_subnet.subnet_c.cidr_block
-}
-
-output "subnet_a_az" {
-  description = "Provides subnet A AZ."
-  value       = aws_subnet.subnet_a.availability_zone
-}
-
-output "subnet_b_az" {
-  description = "Provides subnet B AZ."
-  value       = aws_subnet.subnet_b.availability_zone
-}
-
-output "subnet_c_az" {
-  description = "Provides subnet C AZ."
-  value       = aws_subnet.subnet_c.availability_zone
-}
-
-output "igw_id" {
+output "internet_gateway_id" {
   description = "Provides IGW ID."
   value       = aws_internet_gateway.igw.id
 }
 
-output "rt_id" {
+output "routing_table_id" {
   description = "Provides RT ID."
   value       = aws_route_table.rt.id
 }
